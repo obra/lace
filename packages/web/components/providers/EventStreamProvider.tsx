494be1ba60badbd2f1c19dffb4953c364c233a5f
Jesse Vincent <jesse@fsck.com>
2025-08-25 16:42:27 -0700

    debug: add component render logging to EventStreamProvider
    This will show if EventStreamProvider is even rendering/re-rendering
    when we trigger the error scenario.
    
    EXPECT: Should see '[EventStreamProvider] Component render start'

--- packages/web/components/providers/EventStreamProvider.tsx
+++ packages/web/components/providers/EventStreamProvider.tsx
@@ -85,6 +85,8 @@ export function EventStreamProvider({
   agentId,
   onAgentStateChange,
 }: EventStreamProviderProps) {
+  console.log('[EventStreamProvider] Component render start for session:', sessionId);
+  
   // Get tool approval handlers from ToolApprovalProvider
   const { handleApprovalRequest, handleApprovalResponse } = useToolApprovalContext();
 
