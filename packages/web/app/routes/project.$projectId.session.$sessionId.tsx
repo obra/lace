8d42d1c137e778a3ccd27a3a3ed434c11603344a
Jesse Vincent <jesse@fsck.com>
2025-08-26 07:53:02 -0700

    remove console.log

--- packages/web/app/routes/project.$projectId.session.$sessionId.tsx
+++ packages/web/app/routes/project.$projectId.session.$sessionId.tsx
@@ -7,8 +7,6 @@ import SessionPage from '@/app/project/[projectId]/session/[sessionId]/page';
 export default function ProjectSession() {
   const { projectId, sessionId } = useParams();
 
-  console.log('ProjectSession route called with:', { projectId, sessionId });
-
   // Pass params as props to maintain compatibility
   return <SessionPage params={{ projectId: projectId!, sessionId: sessionId! }} />;
 }
