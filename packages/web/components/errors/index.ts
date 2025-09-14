59cf27963944960890b51c5df8366e668a01a021
Jesse Vincent <jesse@fsck.com>
2025-09-14 16:29:32 -0700

    chore: remove unused error components
    Remove 5 unused error component files that were only imported
    by their own test file:
    - ErrorDisplay, ErrorToast, ErrorLogEntry components
    - Associated test and index files

--- packages/web/components/errors/index.ts
+++
@@ -1,6 +0,0 @@
-// ABOUTME: Centralized exports for error display components
-// ABOUTME: Single import point for all error-related UI components
-
-export { ErrorDisplay } from './ErrorDisplay';
-export { ErrorLogEntry } from './ErrorLogEntry';
-export { ErrorToast } from './ErrorToast';
