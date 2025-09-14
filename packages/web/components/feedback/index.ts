65e173715abadf22df1895c42d11d530a0711725
Jesse Vincent <jesse@fsck.com>
2025-09-14 10:27:39 -0700

    chore: remove unused feedback components
    Remove 13 unused feedback component files that were not referenced
    anywhere in the codebase:
    - FeedbackDisplay, FeedbackEventCard, FeedbackInsightCard
    - FeedbackMiniDisplay, PerformancePanel, PredictivePanel
    - Associated MDX documentation files

--- packages/web/components/feedback/index.ts
+++
@@ -1,9 +0,0 @@
-// ABOUTME: Export barrel for feedback UI components
-// ABOUTME: Centralizes exports for easy importing of feedback components
-
-export { FeedbackDisplay } from './FeedbackDisplay';
-export { FeedbackEventCard } from './FeedbackEventCard';
-export { FeedbackInsightCard } from './FeedbackInsightCard';
-export { PerformancePanel } from './PerformancePanel';
-export { PredictivePanel } from './PredictivePanel';
-export { FeedbackMiniDisplay } from './FeedbackMiniDisplay';
