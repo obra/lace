9175eaf80faf50c46743dc7c3e7ed7edc7645b86
Jesse Vincent <jesse@fsck.com>
2026-05-22 21:00:58 -0700

    chore(notifications): export BuildNotificationOptions type

--- packages/agent/src/notifications/index.ts
+++ packages/agent/src/notifications/index.ts
@@ -1,5 +1,5 @@
 export { buildNotification } from './notification-wrapper';
-export type { NotificationKind } from './notification-wrapper';
+export type { NotificationKind, BuildNotificationOptions } from './notification-wrapper';
 export { injectNotification } from './inject-notification';
 export type { InjectNotificationOptions, IdleWakeHooks } from './inject-notification';
 export * from './composers';
